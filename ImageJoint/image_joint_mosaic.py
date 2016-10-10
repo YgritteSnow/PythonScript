94d449c48b078013887a640c7408acfbb7071969
SKY-20160514MCE\Administrator <773963715@qq.com>
2016-10-10 22:01:59 +0800

    马赛克智能切割和填充

--- ImageJoint/image_joint_mosaic.py
+++ ImageJoint/image_joint_mosaic.py
@@ -6,35 +6,257 @@
 
 import os
 import pygame
+import Image
+
+####################################################
+###
+####################################################
+
+class IntVec3( object ):
+	
+	def __init__(self, x, y=None, z=None):
+		super(IntVec3, self).__init__()
+		if y is None:
+			self._x, self._y, self._z = x
+		else:
+			self._x = x
+			self._y = y
+			self._z = z
+
+	def __eq__(self, b):return self._x == b._x and self._y == b._y and self._z == b._z
+	def __hash__(self):return self._x + self._y * MAX_WIDTH + self._z * MAX_WIDTH * MAX_WIDTH
+	def __str__(self):return "(" + str(self._x) + ", " + str(self._y) + ", " + str(self._z) + ")"
+	def __iter__(self):
+		for i in (self._x, self._y, self._z):yield i
+
+	def __div__(self, b):
+		self._x /= b
+		self._y /= b
+		self._z /= b
+
+	@property
+	def r(self):return self._x
+	@property
+	def g(self):return self._y
+	@property
+	def b(self):return self._y
+
+	def toTuple(self):return (self._x, self._y, self._z)
+	def addTuple(self, (x,y,z)):
+		self._x += x
+		self._y += y
+		self._z += z
+
+########################################################################
+###
+########################################################################
+
+class ImageJointMosaicNode( object ):
+
+	def __init__(self, slicePiece, destImageObj):
+		super(ImageJointMosaicNode, self).__init__()
+
+		self._destColor = None
+		self._sliceSize, self._slicePos = slicePiece
+
+	def _calDatas(self, destImageObj):
+		pixel_sum = IntVec3(0, 0, 0)
+		count = 0
+
+		sizex, sizey = destImageObj.size()
+		x_min, x_max = self._slicePos.x, self._slicePos.x + self._sliceSize.x
+		y_min, y_max = self._slicePos.y, self._slicePos.y + self._sliceSize.y
+		x_min = max(0, x_min)
+		y_min = max(0, y_min)
+		x_max = min(sizex, x_max)
+		y_max = min(sizey, y_max)
+		for x in range(x_min, x_max):
+			for y in range(y_min, y_max):
+				pixel_sum.addTuple(im.getpixel((x,y)))
+				count += 1
+
+		pixel_sum /= count
+
+		self.destColor = pixel_sum
+
+	@property 
+	def destColor(self):return self._destColor
+	@property 
+	def slicePos(self):return self._slicePos
+	@property 
+	def sliceSize(self):return self._sliceSize
+
+########################################################################
+###
+########################################################################
+
+class ImageJointSrcImageNode( object ):
+
+	def __init__(self, imagePath):
+		super(ImageJointSrcImageNode, self).__init__()
+
+		self._color = None
+		self._calDatas(imagePath)
+
+	def _calDatas(self, imagePath):
+		originImage = Image.open(imagePath)
+		pixel_sum = IntVec3(0, 0, 0)
+		count = 0
+
+		for x in range(0, im.size[0]):
+			for y in range(0, im.size[1]):
+				pixel_sum.addTuple(originImage.getpixel((x,y)))
+				count += 1
+		pixel_sum /= count
+
+		self._color = pixel_sum
+
+	@property 
+	def averageColor(self):return self._color
+
+########################################################################
+###
+########################################################################
 
 class ImageJointMosaic( object ):
-    '''
-    计算出一个良好的方案，将给定的若干张图片放入切分好了的目标图片中，尽可能的模拟目标图片的效果
-    '''
+	'''
+	计算出一个良好的方案，将给定的若干张图片放入切分好了的目标图片中，尽可能的模拟目标图片的效果
+	'''
 
-    def __init__(self, destImage, sliceData, srcImageList):
-        super(ImageJointMosaic, self).__init__()
+	def __init__(self, srcImageList, sliceData, destImage):
+		super(ImageJointMosaic, self).__init__()
 
-        self._destImage = destImage
-        self._sliceData = sliceData
-        self._srcImageList = srcImageList
+		self._originImageList = [] # 经过处理的图片的列表
+		self._destMosaicNodes = [] # 经过处理的目标马赛克节点的列表
+		self.SetMosaicData(srcImageList, sliceData, destImage) # 初始化所需的数据
 
-    def _processMosaic(self, destMosaic, srcImageList):
-        '''
-        主计算函数
-        @param destMosaic: 目标图片，格式为：[(切块左上角pos，切块size，切块原color)]
-        @param srcImageList: 源图片，格式为：[imageInfo, ]
-        '''
-        result = []
+		 # RGB通道的权重计算式
+		 # 注意：只支持线性的式子，否则会出现排序错误
+		self._weightMethod = lambda color:color.r + color.g + color.a
 
-        last_color_surplus = 0 # 上一次模拟时的颜色的剩余（已经乘以了面积）
-        for slice_pos, slice_size, slice_color in destMosaic:
-            cur_dest_color = last_color_surplus / slice_size.Area() + slice_color
-            find_src_img = self.FindSlice(cur_dest_color)
-            result.append( find_src_img )
-            last_color_surplus = ( cur_dest_color - find_src_img.Color() ) * find_src_img.Area()
+	####################################################
+	###
+	####################################################
 
-        return result
+	def SetMosaicData(self, srcImageList, sliceData, destImage):
+		self._originImageList = [] # 经过处理的图片的列表
+		self._originImageValueList = [] # 图片的权重值的列表
+		self._destMosaicNodes = [] # 经过处理的目标马赛克节点的列表
 
-    def ProcessMosaic(self):
-        
+		for imagePath in srcImageList:
+			self._originImageList.append(ImageJointSrcImageNode(imagePath))
+
+		self._originImageList.sort(key=lambda node:self._weightMethod(node.averageColor))
+		self._originImageValueList = [self._weightMethod(node.averageColor) for node in self._originImageList]
+
+		destImageObj = Image.open(destImage)
+		for slicePiece in sliceData:
+			self._destMosaicNodes.append(ImageJointMosaicNode(slicePiece, destImageObj))
+
+		return
+
+	####################################################
+	###
+	####################################################
+
+	def ProcessMosaic(self):
+		self._showVisualResult(self._processMosaic())
+
+		return
+
+	def _processMosaic(self, useOrderedMargin=False, canRepeat=True):
+		'''
+		填充马赛克区域
+		@param useOrderedMargin: 是否使用顺序的累加器来累加误差。如果开启的话，建议目标填充物是按照顺序的（或者至少相邻两个在位置上是接近的）
+		@param canRepeat: 是否可以重复选择同一图片
+		@return: 计算结果的图片的列表
+		'''
+		result = []
+
+		repeatBuff = set() if canRepeat else None # 缓存中存储的是图片的index
+
+		last_color_surplus = 0 # 上一次模拟时的颜色的剩余（已经乘以了面积）
+		for slice_node in self._destMosaicNodes:
+			cur_dest_color = slice_node.destColor
+			if useOrderedMargin:cur_dest_color += last_color_surplus / slice_node.sliceSize.Area()
+
+			find_src_img = self.FindMatchImage(cur_dest_color, repeatBuff)
+			result.append( find_src_img )
+
+			last_color_surplus = ( cur_dest_color - find_src_img.Color() ) * find_src_img.Area()
+
+		return result
+
+	####################################################
+	###
+	####################################################
+
+	def FindMatchImage(self, color, repeatBuff=None):
+		if repeatBuff is None:
+			return self._originImageList[self._findMatchImage_repeat_x(color)]
+		else:
+			index = self._findMatchImage_repeat_y(color, repeatBuff)
+			repeatBuff.add(index)
+			return self._originImageList[index]
+
+	def _findMatchImage_repeat_y(self, color, repeatBuff):
+		nearestIdx = self._findMatchImage_repeat_x(color)
+		if nearestIdx not in repeatBuff:return nearestIdx
+
+		destValue = self._weightMethod(color)
+
+		lo = nearestIdx - 1
+		hi = nearestIdx + 1
+		while lo >= 0 or hi < len(self._originImageValueList):
+
+			if not hi < len(self._originImageValueList):
+				while lo in repeatBuff:lo -= 1
+				return lo
+			elif not lo >= 0:
+				while hi in repeatBuff:hi += 1
+				return hi
+
+			if destValue - self._originImageValueList[lo] < self._originImageValueList[hi] - destValue:
+				if lo in repeatBuff:
+					lo -= 1
+				else:
+					return lo
+			else:
+				if hi in repeatBuff:
+					hi += 1
+				else:
+					return hi
+
+	def _findMatchImage_repeat_x(self, color):
+		destValue = self._weightMethod(color)
+
+		# 二分查找落在哪个区间
+		lo = 0 # 左侧的哨兵
+		hi = len(self._originImageValueList) - 1 # 右侧的哨兵
+		while hi - lo > 1:
+			mid = (lo + hi) // 2
+			if self._originImageValueList[mid] > destValue:
+				hi = mid
+			else:
+				lo = mid
+
+		# if hi == len(self._originImageValueList):return lo
+		return hi if self._originImageValueList[hi] - destValue < destValue - self._originImageValueList[lo] else lo
+
+	####################################################
+	###
+	####################################################
+	
+	def _showVisualResult(self, resultImg):
+		pygame.init()
+		screen = pygame.display.set_mode(self.surSize, 0, 32)
+		screen.fill((255, 255, 255))
+
+		for mosaicNode, imageNode in zip(self._destMosaicNodes, resultImg):
+			pygame.draw.rect(screen, imageNode.color.toTuple(), (mosaicNode.slicePos.toTuple(), mosaicNode.sliceSize.toTuple()), 0)
+
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					exit()
+
--- ImageJoint/image_joint_slice.py
+++ ImageJoint/image_joint_slice.py
@@ -1,287 +1,384 @@
 # -*- coding:utf-8 -*-
 # ---------------------------
 # 说明：
-#     图片处理框架
-#     切割图片部分
+#	 图片处理框架
+#	 切割图片部分
 # ---------------------------
 
 import pygame
 import random
 from image_joint_common import *
+import copy
 
 MAX_WIDTH = 9999
 
+Enum_PushResult_Fail = 1
+Enum_PushResult_End = 2
+Enum_PushResult_Success = 3
+
+Enum_ShowRowList = 1
+Enum_ShowSliceData = 2
+
+####################################################
+###
+####################################################
+
 class IntVec2( object ):
-    
-    def __init__(self, x, y=None):
-        super(IntVec2, self).__init__()
-        if y is None:
-            self._x, self._y = x
-        else:
-            self._x = x
-            self._y = y
+	
+	def __init__(self, x, y=None):
+		super(IntVec2, self).__init__()
+		if y is None:
+			self._x, self._y = x
+		else:
+			self._x = x
+			self._y = y
 
-    def __hash__(self):return self._x + self._y * MAX_WIDTH
-    def __str__(self):return "(" + str(self._x) + ", " + str(self._y) + ")"
-    def __iter__(self):
-        for i in (self._x, self._y):yield i
+	def __eq__(self, b):return self._x == b._x and self._y == b._y
+	def __hash__(self):return self._x + self._y * MAX_WIDTH
+	def __str__(self):return "(" + str(self._x) + ", " + str(self._y) + ")"
+	def __iter__(self):
+		for i in (self._x, self._y):yield i
 
-    @property
-    def x(self):return self._x
-    @property
-    def y(self):return self._y
+	@property
+	def x(self):return self._x
+	@property
+	def y(self):return self._y
 
-    def toTuple(self):return (self._x, self._y)
-    def offset(self, (x, y)):return (self._x + x, self._y + y)
+	def toTuple(self):return (self._x, self._y)
+	def offset(self, (x, y)):return (self._x + x, self._y + y)
 
-    def AddX(self, x):self._x += x
-    def AddY(self, y):self._y += y
+	def AddX(self, x):self._x += x
+	def AddY(self, y):self._y += y
 
-        
+	def Area(self):return self._x * self._y
+
+####################################################
+###
+####################################################
+
 class ImageSlicer( object ):
-    '''
-    输入图片的大小和书目，得到合适的图片切割方案
-    0. 输入的内容为“图片大小”到“该大小的图片数量”的字典
-    1. 将所有图片依次放入目标面板中，放置的位置为最小高度的地方
-    2. 如果没有可以放入该高度的地方
-    '''
+	'''
+	输入图片的大小和书目，得到合适的图片切割方案
+	0. 输入的内容为“图片大小”到“该大小的图片数量”的字典
+	1. 将所有图片依次放入目标面板中，放置的位置为最小高度的地方
+	2. 如果没有可以放入该高度的地方
+	'''
 
-    def __init__(self, destSize, imagesInfo=[]):
-        super(ImageSlicer, self).__init__()
-        self._destSize = IntVec2(destSize)
-        self._rowList = [IntVec2(0, 0), ] # [(左下角坐标，宽度), ]
-        self._sliceData = []
+	def __init__(self, destSize, imagesInfo=[]):
+		super(ImageSlicer, self).__init__()
+		self._destSize = IntVec2(destSize)
+		self.clearData()
 
-    def GetSliceRowList(self):
-        return self._rowList
+	def clearData(self):
+		self._rowList = [IntVec2(0, 0), ] # [(左下角坐标，宽度), ]
+		self._sliceData = []
 
-    def GetSliceData(self):
-        return 
+	def GetSliceRowList(self):return self._rowList
+	def GetSliceData(self):return self._sliceData
+	def pushSliceData(self, sliceSize, slicePos):self._sliceData.append((sliceSize, slicePos))
+	def popSliceData(self):self._sliceData.pop()
 
-    def getNeedWidth(self):
-        '''获得下一个准备放置的位置'''
-        minHeight = MAX_WIDTH
-        idx = -1
-        for t_idx, pos in enumerate(self._rowList):
-            if pos.y < minHeight:
-                minHeight = pos.y
-                idx = t_idx
+	####################################################
+	###
+	####################################################
 
-        if idx == -1:
-            raise Exception, "getNeedWidth failed"
-        else:
-            pos = self._rowList[idx]
-            if idx == len(self._rowList) - 1:
-                needWidth = self._destSize.x - pos.x
-            else:
-                needWidth = self._rowList[idx+1].x - pos.x
+	def getNeedWidth(self):
+		'''获得下一个准备放置的位置'''
+		minHeight = MAX_WIDTH
+		idx = -1
+		for t_idx, pos in enumerate(self._rowList):
+			if pos.y < minHeight:
+				minHeight = pos.y
+				idx = t_idx
 
-            return pos, needWidth, idx
+		if idx == -1:
+			raise Exception, "getNeedWidth failed"
+		else:
+			pos = self._rowList[idx]
+			if idx == len(self._rowList) - 1:
+				needWidth = self._destSize.x - pos.x
+			else:
+				needWidth = self._rowList[idx+1].x - pos.x
 
-    def rowPop(self, popSize, popPosX):
-        if popPosX < 0:
-            DEBUG_ERR("Invalid pop x")
-            return
+			return pos, needWidth, idx
 
-        idx = -1
-        for t_idx, pos in enumerate(self._rowList):
-            if pos.x > popPosX:
-                idx = t_idx
-                break
+	def rowPop(self, popSize, popPosX):
+		if popPosX < 0:
+			DEBUG_ERR("Invalid pop x")
+			return
 
-        if idx < 0:# 没有找到，说明在最后切割
-            idx = len(self._rowList)
-            nextX = self._destSize.x
-        else:
-            nextX = self._rowList[idx].x
+		idx = -1
+		for t_idx, pos in enumerate(self._rowList):
+			if pos.x > popPosX:
+				idx = t_idx
+				break
 
-        if idx == 0:
-            # 这意味着self._rowList里边有数据且第一个数据就比popPos大，这是不可能的，因为self._rowList从0开始；
-            # 或者意味着，self._rowList为空，这也是错误的
-            raise Exception, "Poping From Nothing"
+		if idx < 0:# 没有找到，说明在最后切割
+			idx = len(self._rowList)
+			nextX = self._destSize.x
+		else:
+			nextX = self._rowList[idx].x
 
-        if nextX < popPosX + popSize.x:
-            DEBUG_ERR("Cant Pop(for Test)")
-            return
+		if idx == 0:
+			# 这意味着self._rowList里边有数据且第一个数据就比popPos大，这是不可能的，因为self._rowList从0开始；
+			# 或者意味着，self._rowList为空，这也是错误的
+			raise Exception, "Poping From Nothing"
 
-        lastpos = self._rowList[idx-1]
-        if lastpos < popSize.y:
-            DEBUG_ERR("Pop to positive value")
-            return
+		if nextX < popPosX + popSize.x:
+			DEBUG_ERR("Cant Pop(for Test)")
+			return
 
-        self._rowList.insert(idx, IntVec2(popPosX, lastpos.y - popSize.y))
-        self._rowList.insert(idx + 1, IntVec2(popPosX + popSize.x, lastpos.y))
-        self.addSliceData(popSize)
+		lastpos = self._rowList[idx-1]
+		if lastpos < popSize.y:
+			DEBUG_ERR("Pop to positive value")
+			return
 
-        self.simpRowList(idx + 1)
-        self.simpRowList(idx)
+		self._rowList.insert(idx, IntVec2(popPosX, lastpos.y - popSize.y))
+		self._rowList.insert(idx + 1, IntVec2(popPosX + popSize.x, lastpos.y))
 
-        return
+		self.simpRowList(idx + 1)
+		self.simpRowList(idx)
 
-    Enum_PushResult_Fail = 1
-    Enum_PushResult_End = 2
-    Enum_PushResult_Success = 3
+		self.popSliceData()
 
-    def rowPush(self, pushSize):
-        '''
-        增加一个方块，如果成功，返回True和信息，否则返回False和None
-        '''
-        if pushSize.x == 0:
-            DEBUG_ERR("Invalid size x")
-            return
+		return
 
-        (needPosX, minHeight), needWidth, idx = self.getNeedWidth()
+	def rowPush(self, pushSize):
+		'''
+		增加一个方块，如果成功，返回True和信息，否则返回False和None
+		'''
+		if pushSize.x == 0:
+			DEBUG_ERR("Invalid size x")
+			return Enum_PushResult_Fail, None
 
-        if idx == -1:
-            print "cannot put into dest" # 指示压栈尝试错误，需要弹出
-            return Enum_PushResult_Fail, None
-        if minHeight >= self._destSize.y:
-            print "slice end" # 指示递归终点
-            return Enum_PushResult_End, None
+		(needPosX, minHeight), needWidth, idx = self.getNeedWidth()
 
-        newPos = IntVec2(needPosX, minHeight + pushSize.y)
-        if needWidth == pushSize.x:# 恰好与将要放置的位置的宽度相等
-            self._rowList[idx] = newPos
-        elif needWidth > pushSize.x:# 如果容许的空间大于将要放置的位置的宽度
-            self._rowList[idx] = newPos
-            # 如果右侧没有完全超出的话，附加进去
-            if needPosX+pushSize.x < self._destSize.x:
-                self._rowList.insert(idx+1, IntVec2(needPosX+pushSize.x, minHeight))
-        elif idx == len(self._rowList) - 1:# 如果在最右侧，那么不论如何容许放置
-            self._rowList[idx] = newPos
-        else:
-            DEBUG_ERR("didnot match")
-            return Enum_PushResult_Fail, None
+		if idx == -1:
+			print "cannot put into dest" # 指示压栈尝试错误，需要弹出
+			return Enum_PushResult_Fail, None
+		if minHeight >= self._destSize.y:
+			print "slice end" # 指示递归终点
+			return Enum_PushResult_End, None
 
-        self.simpRowList(idx)
-        return Enum_PushResult_Success, needPosX
+		newPos = IntVec2(needPosX, minHeight + pushSize.y)
+		if needWidth == pushSize.x:# 恰好与将要放置的位置的宽度相等
+			self._rowList[idx] = newPos
+		elif needWidth > pushSize.x:# 如果容许的空间大于将要放置的位置的宽度
+			self._rowList[idx] = newPos
+			# 如果右侧没有完全超出的话，附加进去
+			if needPosX+pushSize.x < self._destSize.x:
+				self._rowList.insert(idx+1, IntVec2(needPosX+pushSize.x, minHeight))
+		elif idx == len(self._rowList) - 1:# 如果在最右侧，那么不论如何容许放置
+			self._rowList[idx] = newPos
+		else:
+			DEBUG_ERR("didnot match")
+			return Enum_PushResult_Fail, None
 
-    def simpRowList(self, idx):
-        cur_y = self._rowList[idx].y
-        cur_x = self._rowList[idx].x
-        if len(self._rowList) <= 1:
-            return
+		self.simpRowList(idx)
+		self.pushSliceData(pushSize, IntVec2(needPosX, minHeight))
 
-        while idx < len(self._rowList) - 1: # 向后查找能不能合并
-            t_right = self._rowList[idx+1]
-            if cur_x == t_right.x:
-                self._rowList.pop(idx)
-                break
-            elif cur_y == t_right.y:
-                self._rowList.pop(idx+1)
-            else:
-                break
+		return Enum_PushResult_Success, needPosX
 
-        while idx > 0: # 向前查找能不能合并
-            t_left = self._rowList[idx-1]
-            if cur_x == t_left.x:
-                self._rowList.pop(idx-1)
-                break
-            elif cur_y == t_left.y:
-                self._rowList.pop(idx)
-                idx -= 1
-            else:
-                break
+	def simpRowList(self, idx):
+		cur_y = self._rowList[idx].y
+		cur_x = self._rowList[idx].x
+		if len(self._rowList) <= 1:
+			return
 
-        return
+		while idx < len(self._rowList) - 1: # 向后查找能不能合并
+			t_right = self._rowList[idx+1]
+			if cur_x == t_right.x:
+				self._rowList.pop(idx)
+				break
+			elif cur_y == t_right.y:
+				self._rowList.pop(idx+1)
+			else:
+				break
 
-    def GenerateSlicer(self, imagesInfo, destSize):
-        '''递归压栈和回退，得到误差范围内的一个可行的方案：
-        @param imageInfo: {图片大小:该大小的数量, }
-        @param destSize: 目标大小
-        @param error: 为0
-        '''
-        maxwidth = destSize.x
+		while idx > 0: # 向前查找能不能合并
+			t_left = self._rowList[idx-1]
+			if cur_x == t_left.x:
+				self._rowList.pop(idx-1)
+				break
+			elif cur_y == t_left.y:
+				self._rowList.pop(idx)
+				idx -= 1
+			else:
+				break
 
-        t_stack = []  # [(开始尝试时的idx，尝试的size, 尝试的idx偏移, 尝试的pos),]
-        sizes = imagesInfo.keys()
-        sizesCount = len(sizes)
+		return
 
-        startIdx = random.randint(0, sizesCount)
-        currentIdx = 0
-        while( True ):
-            # 从所有图片中随机选择一个作为下一个起始尝试目标，然后依次遍历所有可尝试的目标，来寻找解决方案
-            # 如果没有找到，那么将上一次的选择pop出去，再继续尝试下一个
+	####################################################
+	###
+	####################################################
 
-            (t_x, minHeight), needWidth, t_idx = self.getNeedWidth()
+	def GenerateSlicer(self, imagesInfo, usageRate = 0.9):
+		'''递归压栈和回退，得到误差范围内的一个可行的方案：
+		@param imageInfo: {图片大小:该大小的数量, }
+		@param usageRate: 所给的图片使用了超过usage百分比，即结束，意味着给定图片不足。如果强行要每个图片都用上的话，时间复杂度会太大
+		'''
+		imageInfo = copy.deepcopy(imagesInfo)
+		originImageCount = 0
+		for i in imagesInfo.values():originImageCount += i
 
-            # 当最小高度达到所需高度的时候结束
-            if minHeight < destSize.y:
-                break
-            
-            while( currentIdx < sizesCount ):
-                imageSize = sizes[(currentIdx + startIdx)%sizesCount]
-                if imageSize.x <= needWidth:
-                pushResult, rowPos = self.rowPush(imageSize)
+		self.clearData()
 
-                if pushResult == Enum_PushResult_Success:
-                    t_stack.append((startIdx, imageSize, currentIdx, rowPos))
-                
-                    startIdx = random.randint(0, sizesCount)
-                    currentIdx = 0
+		maxwidth = self._destSize.x
+		t_stack = []  # [(开始尝试时的idx，尝试的size, 尝试的idx偏移, 尝试的pos),]
+		sizes = imagesInfo.keys()
+		sizesCount = len(sizes)
 
-                    break
+		startIdx = random.randint(0, sizesCount)
+		currentIdx = 0
+		while( True ):
+			# 从所有图片中随机选择一个作为下一个起始尝试目标，然后依次遍历所有可尝试的目标，来寻找解决方案
+			# 如果没有找到，那么将上一次的选择pop出去，再继续尝试下一个
 
-            if currentIdx == sizesCount:
-                if len(t_stack) == 0: # 没有找到解
-                    raise Exception, "no solution found"
-                
-                last_startIdx, last_size, last_sizeIdx, last_pos = t_stack.pop(-1)
-                self.rowPop(last_size, last_pos)
+			(t_x, minHeight), needWidth, t_idx = self.getNeedWidth()
 
-                currentIdx = last_sizeIdx + 1
-                startIdx = last_startIdx
+			# 当最小高度达到所需高度的时候结束
+			if minHeight >= self._destSize.y:
+				print "SUCCESS!"
+				for i,j in imagesInfo.items():print "(", i, ",", j, ")"
+				print "len_sliceData = ", len(self._sliceData)
+				break
+
+			while( currentIdx < sizesCount ):
+				imageSize = sizes[(currentIdx + startIdx)%sizesCount]
+
+				if imagesInfo[imageSize] > 0:
+					pushResult, rowPos = self.rowPush(imageSize)
+					if pushResult == Enum_PushResult_Success:
+						t_stack.append((startIdx, imageSize, currentIdx, rowPos))
+						imagesInfo[imageSize] -= 1 # 把用到的那个方块拿出池子
+					
+						startIdx = random.randint(0, sizesCount)
+						currentIdx = 0
+
+						break
+
+				currentIdx += 1
+
+			if currentIdx == sizesCount:
+				if len(t_stack) == 0: # 没有找到解
+					raise Exception, "no solution found"
+				
+				last_startIdx, last_size, last_sizeIdx, last_pos = t_stack.pop(-1)
+				self.rowPop(last_size, last_pos)
+				imagesInfo[last_size] += 1 # 把原先用到的方块放回池子
+
+				currentIdx = last_sizeIdx + 1
+				startIdx = last_startIdx
+
+			curImageCount = 0
+			for i in imagesInfo.values():curImageCount += i
+			if originImageCount * (1-usageRate) >= curImageCount:
+				print "images is too less:"
+				for i,j in imagesInfo.items():print "(", i, ",", j, ")",
+				print ""
+				raise Exception, "images is too less"
+
+####################################################
+###
+####################################################
 
 class TestImageSlice( object ):
 
-    def __init__(self):
-        super(TestImageSlice, self).__init__()
-        self.imgSize = (400, 200)
-        self.surSize = (410, 210)
+	def __init__(self):
+		super(TestImageSlice, self).__init__()
+		self.imgSize = (400, 200)
+		self.surSize = (410, 210)
+		self._fillColors = []
 
-    def _showSlicer(self, screen, sliceData):
-        screen.fill((255, 255, 255))
-        for idx in range(len(sliceData)-1):
-            pos = sliceData[idx]
-            width = sliceData[idx+1].x - pos.x
-            pygame.draw.line(screen, (0, 0, 255), pos.toTuple(), pos.offset((width, 0)))
-            pygame.draw.line(screen, (0, 0, 255), pos.toTuple(), (pos.x, 0))
-            pygame.draw.line(screen, (0, 0, 255), pos.offset((width, 0)), (pos.x + width, 0))
+	def _showRowList(self, screen, rowListData):
+		screen.fill((255, 255, 255))
+		for idx in range(len(rowListData)-1):
+			pos = rowListData[idx]
+			width = rowListData[idx+1].x - pos.x
+			pygame.draw.line(screen, (0, 0, 255), pos.toTuple(), pos.offset((width, 0)))
+			pygame.draw.line(screen, (0, 0, 255), pos.toTuple(), (pos.x, 0))
+			pygame.draw.line(screen, (0, 0, 255), pos.offset((width, 0)), (pos.x + width, 0))
 
-        if len(sliceData) > 0:
-            pos = sliceData[-1]
-            width = self.imgSize[0] - pos.x
-            pygame.draw.line(screen, (0, 0, 255), pos.toTuple(), pos.offset((width, 0)))
-            pygame.draw.line(screen, (0, 0, 255), pos.toTuple(), (pos.x, 0))
-            pygame.draw.line(screen, (0, 0, 255), pos.offset((width, 0)), (pos.x + width, 0))
+		if len(rowListData) > 0:
+			pos = rowListData[-1]
+			width = self.imgSize[0] - pos.x
+			pygame.draw.line(screen, (0, 0, 255), pos.toTuple(), pos.offset((width, 0)))
+			pygame.draw.line(screen, (0, 0, 255), pos.toTuple(), (pos.x, 0))
+			pygame.draw.line(screen, (0, 0, 255), pos.offset((width, 0)), (pos.x + width, 0))
 
-        pygame.display.update()
+		pygame.display.update()
+
+	def _showSliceData(self, screen, sliceData):
+		screen.fill((255, 255, 255))
+		while len(sliceData) > len(self._fillColors):
+			self._fillColors.append((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+
+		for (sliceSize, slicePos), color in zip(sliceData, self._fillColors):
+			pygame.draw.rect(screen, color, (slicePos.toTuple(), sliceSize.toTuple()), 0)
+
+		pygame.display.update()
+
+	def ShowVisualDataStepByStep(self, rowListData, showType = Enum_ShowRowList):
+		t = ImageSlicer(self.imgSize)
+		#t.rowPush(IntVec2(self.imgSize))
+		pygame.init()
+		screen = pygame.display.set_mode(self.surSize, 0, 32)
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					exit()
+
+				elif event.type == pygame.KEYDOWN:
+					if rowListData:
+						a, b, c = rowListData.pop()
+						t.rowPush(IntVec2(a, b))
+
+						if showType == Enum_ShowRowList:
+							self._showRowList(screen, t.GetSliceRowList())
+						elif showType == Enum_ShowSliceData:
+							self._showSliceData(screen, t.GetSliceData())
+					else:
+						DEBUG_ERR("Done.")
+
+	def ShowVisualDataOnce(self, imagesInfo):
+		t = ImageSlicer(self.imgSize)
+		pygame.init()
+		screen = pygame.display.set_mode(self.surSize, 0, 32)
+		t.GenerateSlicer(imagesInfo)
+		self._showSliceData(screen, t.GetSliceData())
+		print "2222"
+
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					exit()
+		print "3333"
 
 
-    def ShowSlicer(self, testData):
-        t = ImageSlicer(self.imgSize)
-        t.rowPush(IntVec2(self.imgSize))
-        pygame.init()
-        screen = pygame.display.set_mode(self.surSize, 0, 32)
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    exit()
+####################################################
+###
+####################################################
 
-                elif event.type == pygame.KEYDOWN:
-                    if testData:
-                        popsize, posx = testData.pop()
-                        t.rowPop(IntVec2(popsize), posx)
+def main_test():
+	testSlicer = TestImageSlice()
 
-                        self._showSlicer(screen, t.GetSliceRowList())
-                    else:
-                        DEBUG_ERR("Done.")
+	testOpt = 2
+	if testOpt == 1:
+		# 逐步进行的测试模式
+		# 随机生成一些数据来方便测试
+		testNum3List = []
+		for i in range(40):
+			testNum3List.append((random.randint(1, 2) * 20, random.randint(1, 2) * 20, random.randint(1,6)*20))
+		testSlicer.ShowVisualDataStepByStep(testNum3List, showType = Enum_ShowSliceData)
 
-testlist = [
-]
+	elif testOpt == 2:
+		# 直接输入得到解决结果的模式
+		# 随机生成图片的数据的集合
+		testImgList = {}
+		for i in range(40):
+			testImgList[IntVec2(10,20)] = 150
+			testImgList[IntVec2(20,20)] = 150
+			testImgList[IntVec2(20,10)] = 150
+		testSlicer.ShowVisualDataOnce(testImgList)
 
-for i in range(40):
-    testlist.append(((random.randint(1, 2) * 40, random.randint(1, 2) * 40), random.randint(1,6)*40))
-
-tv = TestImageSlice()
-tv.ShowSlicer(testlist)
+main_test()
